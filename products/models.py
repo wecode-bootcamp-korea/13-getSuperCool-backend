31210bf96a0a618a7aaa371fcf8e539b209a25e3
sojeong20 <sojeong.heo@gmail.com>
2020-10-21 17:10:45 +0900

    Modify: 상품 모델 작성 완료 - products/models.py 완료

--- products/models.py
+++ products/models.py
@@ -2,22 +2,31 @@ from django.db import models
 
 class Product(models.Model):
     name            = models.CharField(max_length=45)
-    description     = models.TextField(max_length=1000)
+    description     = models.TextField(max_length=800)
+    super_tip       = models.TextField(max_length=100,null=True)
+    size            = models.CharField(max_length=20)
+    good_to_know    = models.TextField(max_length=200)
+    contains        = models.TextField(max_length=1000)
+    color           = models.CharField(max_length=20,null=True)
     price           = models.IntegerField()
-    ingredients     = models.TextField(max_length=1000)
-    color           = models.CharField(max_length=45, null=True)
-    category1       = models.ForeignKey(Category1, on_delete=models.CASCADE)
+    category1       = models.ForeignKey(Category1,on_delete=models.CASCADE)
+   #review          = models.ForeignKey(reviews,on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products'
 
+class Product_image(models.Model):
+    image_url       = models.URLField(max_length=1000)
+    product         = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product_images'
 
 class Category1(models.Model):
     name            = models.CharField(max_length=10)
 
     class Meta:
         db_table = 'category1'
-
 
 class Category2(models.Model):
     name            = models.CharField(max_length=10)
