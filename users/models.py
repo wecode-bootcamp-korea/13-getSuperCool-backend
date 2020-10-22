b7e0c59b385b4f92553d316fb408077a734021d3
jaeyong <rmfrn2901@naver.com>
2020-10-22 20:14:22 +0900

    Add : models.py user 수정 및 Views.py 수정  models.py user 모든 모델 추가 .

--- users/models.py
+++ users/models.py
@@ -6,6 +6,36 @@ class User(models.Model):
     birth_date  = models.DateField()
     email       = models.EmailField(max_length=255, unique=True)
     password    = models.CharField(max_length=1000)
-    
+    like        =models.ManyToManyField('products.Product' , through='Like')
     class Meta:
         db_table = 'users'  
+
+class Like(models.Model):
+    user    = models.ForeignKey('User', on_delete=models.CASCADE) 
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'likes'
+
+class Subscription(models.Model):
+    email = models.EmailField(max_length=255)
+
+    class Meta:
+        db_table='subscriptions'  
+
+class Inquiry(models.Model):
+    email            = models.EmailField(max_length=255)
+    name             = models.CharField(max_length=45)
+    order_number_id  = models.ForeignKey('products.Product' , on_delete=models.CASCADE,null=True) 
+    country          = models.CharField(max_length=45,null=True)
+    subject_id       = models.OneToOneField('Subject', on_delete=models.CASCADE)
+    message          = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table='inquiries' 
+
+class Subject(models.Model):  
+    subject = models.CharField(max_length=100)
+
+    class Meta:
+        db_table='subjects' 
