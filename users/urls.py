65afc032acd4882a9533e9c3f9284b78e15c9bae
jaeyong <rmfrn2901@naver.com>
2020-10-26 17:08:30 +0900

    modify : urls.py 수정 추가 기능 작업 삭제 .

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views      import SignUpView,LoginView,ForgotPasswordView,InquiriesView,DiscountView,LikeView
+from .views      import SignUpView,LoginView
 
 urlpatterns = [
      path('/register',SignUpView.as_view()),
