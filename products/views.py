f8e628468b67121c04e47d7977a5391290c11919
sojeong20 <sojeong.heo@gmail.com>
2020-10-26 18:43:57 +0900

    Modify: ProductDetailView refactoring

--- products/views.py
+++ products/views.py
@@ -8,24 +8,28 @@ from products.models import Product, Color, ProductImage
 class ProductDetailView(View):
     def get(self, request):
         try:
+
             product_id = request.GET['product_id']
             color_id = request.GET.get('color_id', None)
             
-            if color_id:
-                images = ProductImage.objects.filter(product_id=product_id,color_id=color_id).values_list('image_url',flat=True)
-                other_color_ids = ProductImage.objects.filter(product_id=product_id).values_list('color_id',flat=True).distinct()
-                color_options = [{"id": id, "name": Color.objects.get(id=id).name} for id in other_color_ids]
-            else:
-                images = ProductImage.objects.filter(product_id=product_id).values_list('image_url',flat=True)
-                color_options = []
+            selected_product = Product.objects.get(id=product_id)
 
-            product_info = Product.objects.filter(id=product_id).values()
-            product_images = list(images)
+            color_filter = {'color_id' : color_id}
+            images = selected_product.productimage_set.filter(**color_filter)
+            product_images = [image.image_url for image in images]
             
+            if color_id:
+                color_ids = selected_product.productimage_set.values_list('color_id',flat=True).distinct()
+                color_options = [{"id": id, "name": Color.objects.get(id=id).name} for id in color_ids]
+            else:
+                color_options = []
+            
+            product_info = Product.objects.filter(id = product_id).values()
+
             product_detail = {
+                "product_images" : product_images,     
                 "color_options" : color_options,
-                "product_images" : product_images,
-                "product_info" : product_info[0]
+                "product_info" : product_info[0],
             }
 
             return JsonResponse({"product_detail" : product_detail}, status=200)
