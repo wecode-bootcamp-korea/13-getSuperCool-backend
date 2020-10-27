6eb58e7659d1319195b85b15435f080a2615aae3
jaeyong <rmfrn2901@naver.com>
2020-10-27 21:29:58 +0900

    modify : 로그인 회원가입 수정 - 병민님 리뷰

--- users/views.py
+++ users/views.py
@@ -4,17 +4,19 @@ import re
 import my_settings
 import json
 
+import django
 from django.views import View
 from django.http  import JsonResponse
-import django
 
-from .models      import User,Like,Subscription
+from .models      import User,Like,Subscription,Inquiry
 from products.models import Product
+from  .parameter import authorize_decorator
 
 class SignUpView(View):
     def post(self , request):
         try:
             data = json.loads(request.body)
+
             first_name = data["first_name"]
             last_name  = data["last_name"]
             birth_date = data['birth_date']
@@ -22,26 +24,30 @@ class SignUpView(View):
             password   = data["password"]
 
             pattern=r'[A-Z0-9._%+-]+@[A-Z0-9,-]+\.[A-Z]{2,4}'
-            regex=re.compile(pattern,flags=re.IGNORECASE)
+            regex  = re.compile(pattern,flags=re.IGNORECASE)
             
             if len(regex.findall(email)) == 0:
-                return JsonResponse({"message":"EMAIL_INVALID"})
+                return JsonResponse({"message":"EMAIL_INVALID"},status=400)
             
             if len(password)<8 :
-                return JsonResponse({"message": "PASSWORD_LENGTH_ERROR"})
+                return JsonResponse({"message": "PASSWORD_TOO_SHORT"},status=400)
 
             hash_password   = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
            
-            User.objects.create(first_name=first_name,last_name=last_name,birth_date=birth_date,\
-                                email=email,password=hash_password)
+            User.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                birth_date=birth_date,
+                email=email,password=hash_password
+                )
 
             return JsonResponse({"message":"SUCCESS"},status=200)
 
         except django.db.utils.IntegrityError:
-            return JsonResponse({"message":"DUPLICATE"})
+            return JsonResponse({"message":"DUPLICATE"},status=400)
 
         except KeyError : 
-            return JsonResponse({"message":"KEY_ERROR"})
+            return JsonResponse({"message":"KEY_ERROR"},status=400)
 
 class LoginView(View):
     def post(self,request):
@@ -52,16 +58,15 @@ class LoginView(View):
         
         get_email = User.objects.filter(email=email)
 
-        if len(get_email) > 0 :
-            
-            get_pw_s  = User.objects.get(email=email).password.encode('utf-8')
+        if get_email :
+           
+            get_pw  = User.objects.get(email=email).password.encode('utf-8')
 
-            if bcrypt.checkpw(password.encode('utf-8'), get_pw_s) :
-                access_token    = jwt.encode({'user_id' : get_email[0].id}, my_settings.SECRET['secret'], algorithm = my_settings.ALGORITHM['algorithm'])
-                token=access_token.decode('utf-8') 
-                return JsonResponse ({"message":"SUCCESS",'Authorization':token } )
+            if bcrypt.checkpw(password.encode('utf-8'), get_pw) :
+                access_token    = jwt.encode({'user_id' : get_email[0].id}, my_settings.SECRET['secret'], algorithm = my_settings.ALGORITHM['algorithm']).decode('utf-8') 
+                return JsonResponse ({"message":"SUCCESS",'Authorization':access_token } )
                     
             else :
-                return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"})
+                return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"},status=400)
         else :
-            return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"})
+            return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"},status=400)
