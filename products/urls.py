67f5ad605bc3a2f5b122050d508bd3018a56ae3f
sojeong20 <sojeong.heo@gmail.com>
2020-10-27 16:35:06 +0900

    Add: 상품 리스트 엔드포인트 생성 - ProductListView 생성 - url 수정

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views     import ProductDetailView
+from .views     import ProductDetailView, ProductListView
 
 urlpatterns = [
-        path('/productdetail/<int:product_id>', ProductDetailView.as_view())
+        path('',ProductListView.as_view()),
+        path('<int:product_id>', ProductDetailView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,31 @@
 import json
 
-from django.views import View
-from django.http  import HttpResponse, JsonResponse
+from django.views     import View
+from django.http      import HttpResponse, JsonResponse
+from django.db.models import Q
 
-from products.models import Product, Color, ProductImage
+from products.models import Product, Color, ProductImage, Category, ProductApplyOn, ApplyOn
+
+class ProductListView(View):
+    def get(self, request):
+
+        category_id = request.GET.get('category', '1,2').split(',')
+        apply_on_id = request.GET.get('apply_on','1,2,3').split(',')
+        
+        products_in_category = Product.objects.filter(category_id__in=category_id).values_list('id', flat=True)
+        products_in_apply_on = ProductApplyOn.objects.filter(apply_on_id__in=apply_on_id).values_list('product_id', flat=True)
+        products_in_both     = [id for id in products_in_category if id in products_in_apply_on]
+        products             = Product.objects.filter(id__in=products_in_both)
+
+        product_list = [{
+            'name' : product.name,
+            'price' : product.price,
+            'product_id' : product.id,
+            'color_id' : product.productimage_set.first().color_id,
+            'product_images' : [image_url for image_url in product.productimage_set[:4]]
+            } for product in products]
+        
+        return JsonResponse({"product_list": product_list}, status=200)
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -12,11 +34,11 @@ class ProductDetailView(View):
             
             product = Product.objects.get(id=product_id)
 
-            images = product.productimage_set.filter(color_id=color_id)
+            images         = product.productimage_set.filter(color_id=color_id)
             product_images = [image.image_url for image in images]
             
             if color_id:
-                color_ids = product.productimage_set.values_list('color_id',flat=True).distinct()
+                color_ids     = product.productimage_set.values_list('color_id',flat=True).distinct()
                 color_options = [{"id": id, "name": Color.objects.get(id=id).name} for id in color_ids]
             else:
                 color_options = []
@@ -25,8 +47,8 @@ class ProductDetailView(View):
 
             product_detail = {
                 "product_images" : product_images,     
-                "color_options" : color_options,
-                "product_info" : product_info[0],
+                "color_options"  : color_options,
+                "product_info"   : product_info[0],
             }
 
             return JsonResponse({"product_detail" : product_detail}, status=200)
