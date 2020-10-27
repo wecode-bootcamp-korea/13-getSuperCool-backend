2758641217ece2397ae30db0bf6f87bd8352ead7
jaeyong <rmfrn2901@naver.com>
2020-10-27 16:32:43 +0900

    modify : view 파일 , mysettins.py 수정 - 리뷰 후 수정

--- getsuperfluid/test1.py
+++
@@ -1,7 +0,0 @@
-from parameter import trace
-
-@trace
-def add(a,b):
-    return a+b
-
-print(add(10,20))
--- users/views.py
+++ users/views.py
@@ -1,19 +1,20 @@
-import json
-from django.views import View
-from .models      import User
-from products.models import Product
-from django.http  import JsonResponse
 import bcrypt
 import jwt
-import django
 import re
 import my_settings
+import json
+
+from django.views import View
+from django.http  import JsonResponse
+import django
+
+from .models      import User,Like,Subscription
+from products.models import Product
 
 class SignUpView(View):
     def post(self , request):
         try:
             data = json.loads(request.body)
-            token =request.headers.get('User-Agent')
             first_name = data["first_name"]
             last_name  = data["last_name"]
             birth_date = data['birth_date']
@@ -29,9 +30,11 @@ class SignUpView(View):
             if len(password)<8 :
                 return JsonResponse({"message": "PASSWORD_LENGTH_ERROR"})
 
-            hash_password   = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
-            hash_password_d = hash_password.decode('utf-8')
-            User.objects.create(first_name=first_name,last_name=last_name,birth_date=birth_date,email=email,password=hash_password_d)
+            hash_password   = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+           
+            User.objects.create(first_name=first_name,last_name=last_name,birth_date=birth_date,\
+                                email=email,password=hash_password)
+
             return JsonResponse({"message":"SUCCESS"},status=200)
 
         except django.db.utils.IntegrityError:
@@ -47,20 +50,18 @@ class LoginView(View):
         email      = data["email"]
         password   = data["password"]
         
-        get_email= User.objects.get(email=email).id
-        get_pw_s=User.objects.get(email=email).password
-        get_pw_b=get_pw_s.encode('utf-8')
+        get_email = User.objects.filter(email=email)
+
+        if len(get_email) > 0 :
             
-        if bcrypt.checkpw(password.encode('utf-8'), get_pw_b) :
-            access_token    = jwt.encode({'user_id' : get_email}, my_settings.SECRET['secret'], algorithm = 'HS256') 
-            token=access_token.decode('utf-8') 
-            return JsonResponse ({"message":"SUCCESS",'Authorization':token } )
-                
+            get_pw_s  = User.objects.get(email=email).password.encode('utf-8')
+
+            if bcrypt.checkpw(password.encode('utf-8'), get_pw_s) :
+                access_token    = jwt.encode({'user_id' : get_email[0].id}, my_settings.SECRET['secret'], algorithm = my_settings.ALGORITHM['algorithm'])
+                token=access_token.decode('utf-8') 
+                return JsonResponse ({"message":"SUCCESS",'Authorization':token } )
+                    
+            else :
+                return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"})
         else :
             return JsonResponse({"message":"INVALID EMAIL OR PASSWORD"})
-        
-
-
-     
-
-
