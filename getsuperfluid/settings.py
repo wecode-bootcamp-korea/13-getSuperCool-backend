c49706c581f4e10425428e1ea75abc4529cb7d47
sojeong20 <sojeong.heo@gmail.com>
2020-10-28 19:12:02 +0900

    Modify: prefetch_related, select_related 추가

--- getsuperfluid/settings.py
+++ getsuperfluid/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = my_settings.SECRET['secret']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
@@ -149,3 +149,34 @@ CORS_ALLOW_HEADERS = (
     'x-requested-with',
 	
 )
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'formatters': {
+         'verbose': {
+            'format': '{asctime} {levelname} {message}',
+            'style': '{'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class'     : 'logging.StreamHandler',
+            'formatter' : 'verbose',
+            'level'     : 'DEBUG',
+        },
+        'file': {
+            'level'     : 'DEBUG',
+            'class'     : 'logging.FileHandler',
+            'formatter' : 'verbose',
+            'filename'  : 'debug.log',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers' : ['console','file'],
+            'level'    : 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
--- products/views.py
+++ products/views.py
@@ -7,73 +7,71 @@ from products.models import Product, Color, ProductColor, Image, Category, Produ
 
 class ProductListView(View):
     def get(self, request):
-        
-        category = request.GET.get('category', 'COLOR,CARE').split(',')
-        apply_on = request.GET.get('apply_on','LIPS,EYES,FACE').split(',')
-        
-        category_id = Category.objects.filter(name__in=category).values_list('id',flat=True)
-        apply_on_id = ApplyOn.objects.filter(name__in=apply_on).values_list('id',flat=True)
-
-        products_in_category = Product.objects.filter(category_id__in=category_id).values_list('id', flat=True)
-        products_in_apply_on = ProductApplyOn.objects.filter(apply_on_id__in=apply_on_id).values_list('product_id', flat=True)
-        products_in_both     = [id for id in products_in_category if id in products_in_apply_on]
-
-        products             = Product.objects.filter(id__in=products_in_both)
+        products = Product.objects.all().select_related('category').prefetch_related('productapplyon_set__apply_on','productcolor_set__color')
 
         product_list = [{
-            'name'           : product.name,
-            'price'          : product.price,
-            'product_id'     : product.id,
-            'product_images' : product.productcolor_set.first().image_set.values('product_image', 'model_image')[0], 
-            'color_options'  : [{
+            'name'          : product.name,
+            'price'         : product.price,
+            'product_id'    : product.id,
+            'category'      : product.category.name,
+            'apply_on'      : [applyon.apply_on.name for applyon in product.productapplyon_set.all()],
+            'product_image' : product.productcolor_set.first().image_set.get().product_image,
+            'model_image'   : product.productcolor_set.first().image_set.get().model_image, 
+            'colors' : [{
                 'color_id'   : color.color_id,
                 'color_name' : color.color.name if color.color_id else ''
                 } for color in product.productcolor_set.all()]
             } for product in products]
-        
+            
         return JsonResponse({"product_list": product_list}, status=200)
 
 class ProductDetailView(View):
     def get(self, request, product_id):
         try:
-            product = Product.objects.get(id=product_id)
+            product = Product.objects.prefetch_related('productcolor_set__image_set', 'productcolor_set__color').get(id=product_id)
+
+            product_info = [{ 
+                'name'         : product.name,
+                'description'  : product.description,
+                'super_tip'    : product.super_tip,
+                'size'         : product.size,
+                'good_to_know' : product.good_to_know,
+                'contains'     : product.contains,
+                'price'        : product.price
+                }]
             
-            product_info = Product.objects.filter(id=product_id).values()[0]
             
-            product_colors = product.productcolor_set.all()
-            
-            product_list = [{
-                'color_id'  : product.color_id,
-                'color_name': product.color.name,
-                'images'    : product.image_set.values(
-                    'product_image',
-                    'model_image',
-                    'detail1_image',
-                    'detail2_image'
-                    )[0]
-                } for product in product_colors]
+            color_list = [{
+                'color_id'      : product.color_id,
+                'color_name'    : product.color.name,
+                'product_image' : product.image_set.get().product_image,
+                'model_image'   : product.image_set.get().model_image,
+                'detail1_image' : product.image_set.get().detail1_image,
+                'detail2_image' : product.image_set.get().detail2_image
+                } for product in product.productcolor_set.all()]
 
             product_pairs = Product.objects.filter(category_id=product.category_id).exclude(id=product_id)[:2]
             
             pair_with = [{
-                "name"       : product.name,
-                "price"      : product.price,
-                "image"      : product.productcolor_set.first().image_set.values('product_image')[0],
-                "product_id" : product.id
+                "name"          : product.name,
+                "price"         : product.price,
+                "product_image" : product.productcolor_set.first().image_set.get().product_image,
+                "product_id"    : product.id
                 } for product in product_pairs]
 
             recommendations = Product.objects.all().exclude(id=product_id)
 
             you_may_also_like = [{
-                'name'           : recommendation.name,
-                'price'          : recommendation.price,
-                'product_id'     : recommendation.id,
-                'product_images' : recommendation.productcolor_set.first().image_set.values('product_image', 'model_image')[0],
+                'name'          : recommendation.name,
+                'price'         : recommendation.price,
+                'product_id'    : recommendation.id,
+                'product_image' : recommendation.productcolor_set.first().image_set.get().product_image,
+                'model_image'   : recommendation.productcolor_set.first().image_set.get().model_image,
                 } for recommendation in recommendations]
 
             return JsonResponse({
                 "product_info"      : product_info, 
-                "product_list"      : product_list,
+                "color_list"        : color_list,
                 "pair_with"         : pair_with,
                 "you_may_also_like" : you_may_also_like,
                 }, status=200)
