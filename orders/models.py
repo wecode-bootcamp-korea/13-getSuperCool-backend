42c7c87bcada6d1ffd1e7b822cf1819f63702f23
sojeong20 <sojeong.heo@gmail.com>
2020-10-29 17:42:56 +0900

    저장

--- orders/models.py
+++ orders/models.py
@@ -1,24 +1,24 @@
 from django.db      import models
 
 class Order(models.Model):
-    user            = models.ForeignKey('users.User',on_delete=models.CASCADE)
-    order_number    = models.CharField(max_length=20)
-    order_date      = models.DateField(auto_now_add=True)
-    order_status    = models.ForeignKey('OrderStatus',on_delete=models.CASCADE)
+    user         = models.ForeignKey('users.User',on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=20)
+    order_date   = models.DateField(auto_now_add=True)
+    order_status = models.ForeignKey('OrderStatus',on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'orders'
 
 class OrderStatus(models.Model):
-    status          = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
 
     class Meta:
         db_table = 'order_statuses'
 
 class OrderItem(models.Model):
-    order           = models.ForeignKey('Order',on_delete=models.CASCADE)
-    quantity        = models.IntegerField()
-    product         = models.ForeignKey('products.Product',on_delete=models.CASCADE)
+    order        = models.ForeignKey('Order',on_delete=models.CASCADE)
+    quantity     = models.IntegerField()
+    productcolor = models.ForeignKey('products.ProductColor',on_delete=models.CASCADE,null=True)
 
     class Meta:
         db_table = 'order_items'
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+from products.models import Product
+from orders.models   import Order, OrderItem, OrderStatus
+
+class CartView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        for item in data:
+            product_id
+            color_id
+            quantity
+            price
+            모델 바꾸고(color id 추가)
+
+        order create 
+        user_id
+        order_number
+        order_status_id = 1
+            
+
+        return JsonResponse({"message" : "UPDATED SHOPPING CART"}, status =201)
+
+
+
+
+
